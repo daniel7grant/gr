edaae0c103105b62fc7fc69edc8aff4361a91331
Grant <gergely.daniel7@gmail.com>
2023-01-18 22:26:35 +0100

    Add git2 to get data about current repo

---
+++ src/git/git.rs
@@ -0,0 +1,40 @@
+use color_eyre::{
+    eyre::{eyre, Context, ContextCompat},
+    Result,
+};
+use git2::{BranchType, Repository};
+use std::env;
+
+pub fn get_repository() -> Result<Repository> {
+    let cwd = env::current_dir()?;
+    Repository::open(cwd).wrap_err("There is no git repository in the current directory.")
+}
+
+pub fn get_branch(repo: &Repository) -> Result<String> {
+    let head = repo.head().wrap_err("We are not on a branch currently.")?;
+    let branch_shorthand = head
+        .shorthand()
+        .wrap_err("Branch name is not valid UTF-8.")?;
+    Ok(branch_shorthand.to_string())
+}
+pub fn get_remote_data(repo: &Repository, branch_name: &str) -> Result<(String, String)> {
+    let branch = repo
+        .find_branch(&branch_name, BranchType::Local)
+        .wrap_err(eyre!("Branch {} not found.", &branch_name))?;
+    let upstream_branch = branch.upstream().wrap_err(eyre!(
+        "Branch {} doesn't have an upstream branch.",
+        &branch_name
+    ))?;
+    let upstream_branch_name = upstream_branch
+        .name()?
+        .wrap_err("Remote branch name is not valid UTF-8.")?;
+    let (remote_name, remote_branch) = upstream_branch_name.split_once('/').wrap_err(eyre!(
+        "Remote branch name {} is invalid.",
+        upstream_branch_name
+    ))?;
+    let remote = repo
+        .find_remote(remote_name)
+        .wrap_err(eyre!("Remote URL with name {} not found.", remote_name))?;
+    let remote_url = remote.url().wrap_err("Remote URL is not valid UTF-8.")?;
+    Ok((remote_url.to_string(), remote_branch.to_string()))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 mod cmd;
+mod git;
 
 use cmd::config::Configuration;
 use color_eyre::eyre::{eyre, ContextCompat};
 use color_eyre::Result;
-use gr::git::url::parse_url;
+use git::{git::{get_branch, get_remote_data, get_repository}, url::parse_url};
 use gr::vcs::common::{init_vcs, CreatePullRequest};
 
 #[tokio::main]
@@ -12,7 +13,11 @@ async fn main() -> Result<()> {
 
     let conf = Configuration::new()?;
 
-    let (hostname, repo) = parse_url("git@gitlab.danielgrants.com:grant/gr.git")?;
+    let repo = get_repository()?;
+    let branch = get_branch(&repo)?;
+    let (url, branch) = get_remote_data(&repo, &branch)?;
+    
+    let (hostname, repo) = parse_url(&url)?;
     let vcs_type = conf.find_type(&hostname);
     let auth = conf.find_auth(&hostname, &repo).wrap_err(eyre!(
         "Authentication not found for {} {}.",
@@ -21,17 +26,17 @@ async fn main() -> Result<()> {
     ))?;
     let vcs = init_vcs(hostname, repo, auth, vcs_type);
 
-    let _ = vcs
-        .create_pr(CreatePullRequest {
-            title: "asd".to_string(),
-            description: "asd".to_string(),
-            source: "feature/test".to_string(),
-            target: "master".to_string(),
-            close_source_branch: true,
-        })
-        .await?;
+    // let _ = vcs
+    //     .create_pr(CreatePullRequest {
+    //         title: "asd".to_string(),
+    //         description: "asd".to_string(),
+    //         source: "feature/test".to_string(),
+    //         target: "master".to_string(),
+    //         close_source_branch: true,
+    //     })
+    //     .await?;
 
-    let pr = vcs.get_pr("feature/test").await?;
+    let pr = vcs.get_pr(&branch).await?;
     println!("{:#?}", pr);
     Ok(())
 }
