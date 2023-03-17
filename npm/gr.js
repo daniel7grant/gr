4469404854baf54e96ab68abd50e7527c3d365db
Grant <gergely.daniel7@gmail.com>
2023-03-17 14:23:56 +0100

    Change installation path to global

--- npm/gr.js
+++ npm/gr.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
 const { spawn } = require('child_process');
+const path = require('path');
 
-spawn('./gr', process.argv.slice(2), { stdio: 'inherit' });
+spawn(path.join(__dirname, 'gr'), process.argv.slice(2), { stdio: 'inherit' });
