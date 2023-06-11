430e48d409b9e49643c6f00551ac0b55d254340a
Grant <gergely.daniel7@gmail.com>
2023-06-11 18:44:41 +0200

    Add a little more waiting to fork

--- src/cmd/repo/fork.rs
+++ src/cmd/repo/fork.rs
@@ -71,6 +71,7 @@ pub fn fork(args: Cli, mut conf: Configuration) -> Result<()> {
                     }
                 }
             }
+			sleep(Duration::from_millis(1000));
 
             // If clone is given, clone it to the directory (or here)
             let repository = LocalRepository::init(dir.clone())?;
